ab29bd1e13dc264a2efd95e038588f113365aaf8
kCottrell-dev <73491964+kCottrell-dev@users.noreply.github.com>
2021-04-06 23:10:27 -0500

    Update ArrayListSort.java

--- ArrayListSort.java
+++ ArrayListSort.java
@@ -11,20 +11,20 @@ import java.util.ArrayList;
 public class ArrayListSort {
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>(); //Create Arraylist
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) { //Int For loop
             list.add(i);
         }
         System.out.println(list);
-        ArrayListShuffle.shuffle(list);
+        ArrayListShuffle.shuffle(list); //Print Lists
         System.out.println(list);
         sort(list);
         System.out.println(list);
 
     }
 
-    public static <E extends Comparable<E>> void sort(ArrayList<E> list) {
+    public static <E extends Comparable<E>> void sort(ArrayList<E> list) { // Sort Method
 
 
         for (int i = 0; i < list.size() - 1; i++) {
